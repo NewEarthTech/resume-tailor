daf5fa1ca7035c85b5c98ea9d7fe56bfe8caa8df
John Carmack <johncarmack@me.com>
2023-10-29 10:17:52 -0700

    Add link button to resume ID in data table

--- src/app/(main)/resume/columns.tsx
+++ src/app/(main)/resume/columns.tsx
@@ -1,15 +1,27 @@
 "use client";
 
+import Link from "next/link";
 import type { Resume } from "@/db/schema/resume";
 import { ColumnDef } from "@tanstack/react-table";
 import { MoreHorizontal } from "lucide-react";
 
 import { DataTableColumnHeader } from "@/components/content/data-table/column-header";
+import { Button } from "@/components/ui/button";
 
 export const columns: ColumnDef<Resume>[] = [
   {
     accessorKey: "id",
     header: "ID",
+    cell: ({ row }) => (
+      <Button
+        key={row.getValue("id")}
+        className="max-w-fit overflow-hidden text-sm"
+        asChild
+        variant="ghost"
+      >
+        <Link href={`/resume/${row.getValue("id")}`}>{row.getValue("id")}</Link>
+      </Button>
+    ),
   },
   {
     accessorKey: "user_title",
