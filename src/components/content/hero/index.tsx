4ed427d7fd85f60a0730b33840c1a86d7899229a
John Carmack <johncarmack@me.com>
2023-10-29 00:13:09 -0700

    Update hero component styles and button variant

--- src/components/content/hero/index.tsx
+++ src/components/content/hero/index.tsx
@@ -14,15 +14,10 @@ export function Hero() {
         className="object-[center_bottom] transition-transform duration-1000 ease-in-out group-hover:scale-105"
         objectFit="cover"
       />
-      <div className="relative flex h-full flex-col items-center justify-center text-white backdrop-brightness-50 transition-[backdrop-filter] duration-1000 ease-in-out group-hover:backdrop-brightness-[.65]">
+      <div className="relative flex h-full flex-col items-center justify-center text-white backdrop-brightness-50 transition-[backdrop-filter] duration-1000 ease-in-out group-hover:backdrop-brightness-[.65] dark:backdrop-brightness-75">
         <h1 className="mb-2 text-4xl font-bold">Resume Tailor</h1>
         <p className="mb-12 text-xl">Craft your perfect resume in no time</p>
-        <Button
-          className="mx-auto py-7"
-          variant="destructive"
-          size="lg"
-          asChild
-        >
+        <Button className="mx-auto py-7" variant="secondary" size="lg" asChild>
           <Link href="/sign-up">Get Started</Link>
         </Button>
       </div>
