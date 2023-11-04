715aeceb544e5231cb1c4dfc81fd1388324a38b9
John Carmack <johncarmack@me.com>
2023-11-04 16:04:10 -0700

    Update package versions and refactor db connection code

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,4 +2,4 @@ import { sql } from "@vercel/postgres";
 import { drizzle } from "drizzle-orm/vercel-postgres";
 
 // Connect drizzle to Vercel Postgres
-export const db = drizzle(sql, { logger: true });
+export const db = drizzle(sql as never, { logger: true });
--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -2,6 +2,6 @@ import { sql } from "@vercel/postgres";
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import { migrate } from "drizzle-orm/vercel-postgres/migrator";
 
-const db = drizzle(sql, { logger: true });
+import { db } from "./index";
 
 await migrate(db, { migrationsFolder: "src/db/migrations" });
