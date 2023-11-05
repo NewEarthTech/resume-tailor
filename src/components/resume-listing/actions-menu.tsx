3e420566369c507f59ef45f9c23bfb7200edd901
John Carmack <johncarmack@me.com>
2023-11-05 07:31:16 -0800

    Add tooltip functionality to resume listing actions

--- src/components/resume-listing/actions-menu.tsx
+++ src/components/resume-listing/actions-menu.tsx
@@ -1,5 +1,6 @@
 import deleteResume from "@/db/actions/resume/delete";
 import { Resume } from "@/db/schema/resume";
+import { TooltipTrigger } from "@radix-ui/react-tooltip";
 import { type Row } from "@tanstack/react-table";
 import { useCopyToClipboard } from "@uidotdev/usehooks";
 import { Copy, Delete, Download, MoreHorizontal, Share } from "lucide-react";
@@ -15,6 +16,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Separator } from "@/components/ui/separator";
 import { toast, useToast } from "@/components/ui/use-toast";
+import { Tooltip, TooltipContent, TooltipProvider } from "../ui/tooltip";
 
 export function ActionsMenu({ row }: { row: Row<Resume> }) {
   const [copiedText, copyToClipboard] = useCopyToClipboard();
@@ -51,32 +53,36 @@ export function ActionsMenu({ row }: { row: Row<Resume> }) {
 
   return (
     <>
-      <div className="hidden items-center justify-end 2xl:flex">
+      <div className="hidden items-center justify-end xl:flex">
         {actions.map(({ label, Icon, action }, i) => {
           if (!action)
             return (
               <Separator
                 key={`mobile-seperator-${i}`}
-                className="mx-2 h-6"
+                className="mx-2 h-5"
                 orientation="vertical"
               />
             );
           return (
-            <Button
-              key={`resume-listing-item-action-${i}`}
-              // className="flex cursor-pointer items-center gap-2"
-              onClick={action}
-              variant="ghost"
-              size="icon"
-            >
-              {Icon ? <Icon /> : null}
-              {/* {label} */}
-            </Button>
+            <TooltipProvider key={`resume-listing-item-action-${i}`}>
+              <Tooltip>
+                <Button
+                  onClick={action}
+                  variant="ghost"
+                  className="mx-1 aspect-square"
+                  size="icon"
+                  asChild
+                >
+                  <TooltipTrigger>{Icon ? <Icon /> : null}</TooltipTrigger>
+                </Button>
+                <TooltipContent>{label}</TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           );
         })}
       </div>
       <DropdownMenu>
-        <DropdownMenuTrigger asChild className="block 2xl:hidden">
+        <DropdownMenuTrigger asChild className="block xl:hidden">
           <Button variant="ghost" className="h-8 w-8 p-0">
             <span className="sr-only">Open menu</span>
             <MoreHorizontal className="h-4 w-4" />
